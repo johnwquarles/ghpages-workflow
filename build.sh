d8ef9c5c390aac947b4d12ffb7bb2a3338976d3c
johnwquarles <john.w.quarles@gmail.com>
2015-05-13 11:53:23 -0500

    build script has been written

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+rm -rf public
+mkdir public
+
+# compile jade to html
+./node_modules/.bin/jade src -P
+cd src
+find . -name "*.html" | cpio -pdvm ../public
+cd ..
+rm -rf src/**/*.html
+rm -rf public/**/_*.html
+rm -rf public/_partials
+
+# compile sass to css
