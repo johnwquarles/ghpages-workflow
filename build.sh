49a4a77daa41a0c21c12ff11a7c3ca33cdf2bba0
johnwquarles <john.w.quarles@gmail.com>
2015-05-14 13:59:28 -0500

    finalizing this

--- build.sh
+++ build.sh
@@ -9,14 +9,8 @@ rm -rf public
 cp -r src public
 
 # compile jade to html
-./node_modules/.bin/jade src -P
-cd src
-find . -name "*.html" | cpio -pdvm ../public
-cd ..
-rm -rf src/*.html \
-       src/**/*.html \
-       public/**/_*.html \
-       public/_partials
+./node_modules/.bin/jade src -o public -PH
+rm -rf public/_partials
 
 # compile sass to css
 ./node_modules/.bin/node-sass \
