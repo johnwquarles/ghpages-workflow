9c16d40155eded0fdfed734dc258109af3ba1469
johnwquarles <john.w.quarles@gmail.com>
2015-05-26 11:33:23 -0500

    added babel, jade, and sass grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,30 @@
 module.exports = function(grunt) {
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
+  // grunt.loadNpmTasks('grunt-contrib-clean');
+  // grunt.loadNpmTasks('grunt-contrib-copy');
+  // grunt.loadNpmTasks('grunt-contrib-jade');
+  // grunt.loadNpmTasks('grunt-babel');
+  // grunt.loadNpmTasks('grunt-sass');
 
+  require('load-grunt-tasks')(grunt);
 
-  // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    babel: {
+    	main: {
+    		options: {
+    			sourceMap: 'inline'
+    		},
+    		files: [
+    			{
+            expand: true,
+            cwd: 'src/',
+            src: ['**/*.js'],
+            dest: 'public/'
+          }
+    		]
+    	}
+    },
     clean: ['public'],
     copy: {
       main: {
@@ -20,14 +37,40 @@ module.exports = function(grunt) {
           }
         ]
       }
+    },
+    jade: {
+      main: {
+        options: {
+          pretty: true
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'src/',
+            src: ['**/*.jade', '!**/_*.jade'],
+            dest: 'public/',
+            ext: '.html'
+          }
+        ]
+      }
+    },
+    sass: {
+    	main: {
+    		options: {
+    			sourceMap: true,
+    			sourceMapEmbed: true
+    		},
+    		files: {
+    			'public/css/main.css': 'src/_styles/main.scss'
+    		}
+    	}
     }
   });
 
-  // Default task(s).
   grunt.registerTask('default', []);
   grunt.registerTask('build', [
-      'clean',
-      'copy'
-  	]);
+    'clean',
+    'copy'
+  ]);
 
 };
